a7597eadb1cba45404c33837a231b64b577302b8
Andreas Bruse <andreas@bruse.se>
2014-10-09 11:34:06 +0000

    Make rust count vector fixed sized instead of dynamic

--- vector_grow_rust.rs
+++ vector_grow_rust.rs
@@ -9,15 +9,14 @@ fn main() {
     let args = os::args();
     let n: uint = from_str(args[1].as_slice()).expect("Bad argv");
     let mut v = Vec::new();
-    let mut m = Vec::with_capacity(101);
+    let mut m: [u64, ..101] = [0, ..101];
 
     let t = time::precise_time_ns();
-    m.push(0);
-    for _i in iter::range_step(0, n, n/100) {
+    for i in iter::range_step(0, n, n/100) {
         for j in range(0, n/100) {
             v.push(j);
         }
-        m.push(time::precise_time_ns() - t);
+        m[i / (n / 100) + 1] = time::precise_time_ns() - t;
     }
 
     println!("\"size\",\"time\"");
